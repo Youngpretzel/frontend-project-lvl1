6551172645f9d543aa13e973bb90c0aab4f025ea
YoungPretzel <youngpretzel@yandex.ru>
2020-06-12 17:59:39 +0300

    refactor code "brain-progression"

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,14 +2,15 @@ import { generateRandomNum } from '../utilits.js';
 import playBrainGame from '../index.js';
 
 const condition = 'What number is missing in the progression?';
-const countNumProgression = 10;
+const progressionLength = 10;
 
 const getProgression = () => {
   const firstNum = generateRandomNum(0, 50);
   const interval = generateRandomNum(1, 10);
   const arithmeticProgression = [firstNum];
-  for (let i = 1; i < countNumProgression; i += 1) {
-    const nextNum = arithmeticProgression[i - 1] + interval;
+  for (let i = 1; i < progressionLength; i += 1) {
+    const previousNumber = arithmeticProgression[i - 1];
+    const nextNum = previousNumber + interval;
     arithmeticProgression.push(nextNum);
   }
   return arithmeticProgression;
