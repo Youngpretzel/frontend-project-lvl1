a71b1b17fe9e38cd93cdb5bc1dd43196294cfcc7
YoungPretzel <youngpretzel@yandex.ru>
2020-06-18 17:29:24 +0300

    refactor all code

--- src/index.js
+++ src/index.js
@@ -5,17 +5,16 @@ const countRound = 3;
 const playBrainGame = (getQuestionAnswer, condition) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I Have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(condition);
   for (let i = 0; i < countRound; i += 1) {
     const [question, answer] = getQuestionAnswer();
     const userAnswer = readlineSync.question(`Question: ${question} \nYour answer: `);
-    if (answer === userAnswer) {
-      console.log('Correct!');
-    }
     if (answer !== userAnswer) {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".\nLet's try again, ${userName}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${userName}!`);
 };
